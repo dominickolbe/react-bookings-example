8c8df786c3c7f4e31099494922a6254344b3f236
Dominic Kolbe <hi@dominickolbe.dk>
2018-09-16 16:42:48 +0200

    initial setup

--- README.md
+++
@@ -1,2 +0,0 @@
-# react-bookings-example
-This is an example react project to handle bookings.
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+
+    const testvar = 'ABC';
+
+    return (
+      <div className="App">
+        {testvar}
+      </div>
+    );
+  }
+}
+
+export default App;
